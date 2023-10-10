c92d5c71688106aed87b2de0769b54695d04577e
Haariz-H <hasanhaariz619@gmail.com>
2023-10-10 15:59:06 +0530

    Frontend Added

---
+++ app/frontend/src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import "./App.css";
+import { Route, Link, Routes } from "react-router-dom";
+import MenuBar from "./components/MenuBar";
+import CreateEmployee from "./pages/CreateEmployee";
+import ListEmployee from "./pages/ListEmployee";
+import EditEmployee from "./pages/EditEmployee";
+
+function App() {
+  return (
+    <div>
+      <MenuBar></MenuBar>
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<CreateEmployee />}></Route>
+          <Route path="/create-employee" element={<CreateEmployee />}></Route>
+          <Route path="/list-employee" element={<ListEmployee />}></Route>
+          <Route path="/edit-employee" element={<EditEmployee />}></Route>
+        </Routes>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ app/frontend/src/pages/CreateEmployee.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function CreateEmployee() {
+  return (
+    <div>
+      <h1>Create Employee</h1>
+    </div>
+  );
+}
+
+export default CreateEmployee;
