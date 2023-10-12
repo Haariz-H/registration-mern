8d7922071c73fbfba970031cc4ab1f0633878852
Haariz-H <hasanhaariz619@gmail.com>
2023-10-12 13:09:50 +0530

    Employee List Added

--- app/backend/routes/employeeroutes.js
+++ app/backend/routes/employeeroutes.js
@@ -3,16 +3,46 @@ let mongoose = require("mongoose"),
   router = express.Router();
 let employeeSchema = require("../models/Employee");
 
-router.route("/create-employee").post(async (req, res, next)=>{
-    await employeeSchema.create(req.body).
-    then((result)=>{
-        res.json({
-            data:result,
-            message:"Data Successfully added",
-            status:200,
-                })
-    }).catch((err)=>{
-        returnnex(err);
+router.route("/create-employee").post(async (req, res, next) => {
+  await employeeSchema
+    .create(req.body)
+    .then((result) => {
+      res.json({
+        data: result,
+        message: "Data Successfully added",
+        status: 200,
+      });
     })
+    .catch((err) => {
+      returnnex(err);
+    });
 });
-module.exports=router;
+
+router.route("/").get(async (req, res, next) => {
+  await employeeSchema
+    .find()
+    .then((result) => {
+      res.json({
+        data: result,
+        message: "All items successfully fetched",
+        status: 200,
+      });
+    })
+    .catch((err) => {
+      return next(err);
+    });
+});
+router.route("/delete-employee/:id").delete(async (req, res, next) => {
+  await employeeSchema
+    .findByIdAndRemove(req.params.id)
+    .then(() => {
+      res.json({
+        msg: "Data Successfully updated",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
+
+module.exports = router;
